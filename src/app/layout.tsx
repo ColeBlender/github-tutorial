8daf23a2531d6550818ca50475220d030f17d971
Cole Blender <coleblender@gmail.com>
2024-04-20 10:28:49 -0700

    made change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        Dank
+        Dank Cool
       </body>
     </html>
   );
